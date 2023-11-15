0418a30736ec43f93fcfbd4c37ebddb399f980f3
Olseda20 <49950899+Olseda20@users.noreply.github.com>
2023-11-15 16:55:10 +0000

    new run.py

--- src/run.py
+++ src/run.py
@@ -1,7 +1,7 @@
-from ctypes import util
-from logging import config
+from time import sleep
+# from pgmpy import config
 from py import test
-# import utils
+import utils
 import os
 from matplotlib import pyplot as plt
 import networkx as nx
@@ -11,37 +11,48 @@ from CPT_Generator import CPT_Generator
 from NB_Classifier import NB_Classifier as nbc
 from BayesNetInference import BayesNetInference as bni
 from GaussianProcess import GaussianProcess as gp
+from ModelEvaluator import ModelEvaluator as me
 
 run_path = os.path.dirname(os.path.realpath(__file__))
 
-def pc_stable():
-    # PCStable = PCStable('data/lung_cancer-train.csv')
-    pcs_test = pcs('data/cardiovascular_data-discretized-train.csv', method='chisq', independence_threshold=0.05)
-    pcs_test.evaluate_skeleton(with_plots= True,log_level=1)
-    pcs_test.evaluate_immoralities()
-    print('something cool')
+def get_naive_bayes_struct(data_train: str, data_test: str, target_value: str):
+    # data_train = 'data/diabetes_data-discretized-train.csv'
+    # target_value = 'Outcome'
+    training_data = utils.read_training_data(data_train, target_value)
+    
+    
+    # nb_fitted = nbc(data_train)
+    # nb_tester = nbc(data_test, nb_fitted)
+    
+    # get the structure of the naive bayes classifier
+    get_naive_bayes_struct = utils.independent_probability_structure(training_data)
+    structure_data, structure_array = get_naive_bayes_struct
+    print(structure_array)
+    
+    # generate config file with structure
+    config_path = utils.config_structure_file(structure_array, 'nb-diabetes-structure', 'run_test')
+    return config_path
+
+    
+
+def get_pc_stable_structure(data_train, method='chisq', independence_threshold=0.05):
+    pcs_test = pcs(data_train, method, independence_threshold)
+    pcs_test.evaluate_skeleton(with_plots= False,log_level=1)
+    # pcs_test.evaluate_immoralities()
     # pcs_test.create_directional_edge_using_immorality()
-    # print(len(pcs_test.get_graph_edges))
     
-    rad_dag = pcs_test.randomised_directed_graph()
-    nx.draw_shell(rad_dag, with_labels=True)
-    plt.show()
-    print('something cool')
-    # nx.topological_sort(rand_dag)
+    rand_dag = pcs_test.randomised_directed_graph()
+    # nx.draw_shell(rand_dag, with_labels=True)
+    # plt.show()
+    
+    # # creating the config file
+    data = utils.topological_sort_for_structure(pcs_test.nodes, rand_dag)
+    config_path = utils.config_structure_file(data, 'pc-diabetes-structure', 'run_test')
+    
+    return config_path
 
-    # # Step 2: Identify the immoralities (v-structures) and orient them
-    # self.identifyImmoralities()
-    
-    # # Step 3: Identify the remaining edges and orient them
-    # self.identifyQualifyingEdges()
-    
-    # produce config file with the structure of the graph
-    # util.formatIntoConfigStructureFile() <- check if this will work?
-
-    # run inference 
     
 def nb_classifier(train_file, test_file):
-
     nb_fitted = nbc(train_file)
     if test_file is not None:
         nb_tester = nbc(test_file, nb_fitted)
@@ -49,7 +60,6 @@ def nb_classifier(train_file, test_file):
     return nb_fitted
     # we assume that all the data is independent of each other because it is a naive bayes classifier
     
-
 def bayes_net_exact_inference(config_file, prob_query, num_samples):
     # run this after creating the config file for the specific bayes net
     # we can only run an inference method with parents of the variable we are interested in?
@@ -72,17 +82,6 @@ def rejection_sampling(config_file, prob_query, num_samples):
     )
     return rejection_sampling
     
-    
-def cpt_generator():
-    # data_path = f"{run_path}/../docs/workshops/w2/data/play_tennis-train.csv"
-    # evaluation_variable = 'PT'
-    # config_file_path = utils.formatIntoConfigStructureFile("PT",data_path)
-    
-    config_file_path = "config/config-pt-created.txt"
-    training_data = "data/play_tennis-train.csv"
-    CPT_Generator(config_file_path, training_data)
-    
-    
 def gaussian_process(train_data, test_data):
     gaussian_process = gp(train_data, test_data)
     return gaussian_process
@@ -92,16 +91,38 @@ def gaussian_process(train_data, test_data):
 if __name__ == "__main__":
     train_data = 'data/diabetes_data-discretized-train.csv'
     test_data = 'data/diabetes_data-discretized-test.csv'
-    config_file = 'config/config-lungcancer.txt'
+    # config_file = 'config/config-lungcancer.txt'
+    target_value = 'Outcome'
 
+    #############################
+    #### Structure Generator ####
+    #############################
     
-    pc_stable()
+    # config_file = get_naive_bayes_struct(train_data,test_data,target_value)
+    # config_file = get_pc_stable_structure(train_data, method='chisq', independence_threshold=0.05)
     
-    # run_nb_classifier = nb_classifier('data/diabetes_data-discretized-train.csv', 'data/diabetes_data-discretized-test.csv')
+    
+    
+    #############################
+    ###### CPT Generator ########
+    #############################
+    # CPT_Generator(config_file, train_data)
+    
+    
+    
+    # run_nb_classifier = nb_classifier(train_data, test_data)
+    # print(run_nb_classifier.estimate_probabilities)
+    
+    
+    ###################################
+    ###### Inference Generator ########
+    ###################################
     
     # # we can only run an inference method with parents of the variable we are interested in?
     # exact_inference = bayes_net_exact_inference(config_file, 'P(Yellow_Fingers|Lung_cancer=0)', 1000)   
-     
+    
+    config_file = 'config/config-pc-diabetes-structure-run_test-11-15_16:36:13.txt'
+    evaluator = me(config_file, train_data,test_data, True)
      
     # # gauss_proc = gaussian_process('data/diabetes_data-original-train.csv', 'data/diabetes_data-original-test.csv')
     
